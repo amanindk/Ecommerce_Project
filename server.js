d6a1b2fca15ecda8df4e0a8fabc95389522e847c
Amandeep <amandeepsingh57537@gmail.com>
2024-02-18 15:59:06 +0530

    6th change path in server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import { fileURLToPath } from "url";
 
 //rest api
 const app = express();
-const path = require("path");
+// const path = require("path");
 const express = require('express');
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
