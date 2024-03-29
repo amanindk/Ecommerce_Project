4a9b7657eba61fd6b091619345cd91115fb56acc
Amandeep <amandeepsingh57537@gmail.com>
2024-03-29 12:11:32 +0530

    changing deploy

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(bodyParser.json({ limit: '50mb' }));
 
-// app.get("/", (req, res) => {
-//   app.use(express.static(path.resolve(__dirname, "frontend", "dev")));
-//   res.sendFile(path.resolve(__dirname, "frontend", "dev", "index.html"));
-// });
+
 
 //configure env
 dotenv.config();
